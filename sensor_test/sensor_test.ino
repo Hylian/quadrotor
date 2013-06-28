cc30ab7af5404442fd7c7c4c0b2f388948608242
Edward Shin <edshin95@gmail.com>
2013-06-27 17:43:47 -0700

    Modified sensor testing code (all 3 axis of accel now return same value)

--- sensor_test/sensor_test.ino
+++ sensor_test/sensor_test.ino
@@ -3,10 +3,19 @@
 int accel[3];
 int gyro[3];
 
+//Accelerometer Address: 0x19
+//Gyro Address: 0x1E?
+
 void setup()
 {
   Serial.begin(9600);
   Wire.begin();
+  
+  //Enable Accel
+  Wire.beginTransmission(0x19);
+  Wire.write(0x20);
+  Wire.write(0x27);
+  Wire.endTransmission();
 }
 
 void loop()
@@ -17,7 +26,7 @@ void loop()
    Wire.requestFrom(0x1E, 6);
    for(char i = 0; i < 3; i++)
    {
-     accel[i] = Wire.read() << 8 | Wire.read();
+     gyro[i] = Wire.read()*256 + Wire.read();
    }
    
    Wire.beginTransmission(0x19);
@@ -26,7 +35,8 @@ void loop()
    Wire.requestFrom(0x19, 6);
    for(char i = 0; i < 3; i++)
    {
-     gyro[i] = Wire.read() << 8 | Wire.read();
+     while(Wire.available()==0){}
+     accel[i] = Wire.read()*256 + Wire.read();
    }
    
    Serial.println();
