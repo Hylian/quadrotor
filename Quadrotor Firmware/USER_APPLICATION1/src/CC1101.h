32c0a209ad806feae895d81e761ca5487beefe9b
Edward S. <edshin95@gmail.com>
2013-06-30 10:37:21 -0400

    Changed register settings according to SmartRF and fixed trailing #endif

--- Quadrotor Firmware/USER_APPLICATION1/src/CC1101.h
+++ Quadrotor Firmware/USER_APPLICATION1/src/CC1101.h
@@ -4,53 +4,53 @@
 #define CC1101_PA_TABLESIZE 8
 
 // CC1101 STROBE, CONTROL AND STATUS REGISTER
-#define CC1101_IOCFG2       0x00        // GDO2 output pin configuration
-#define CC1101_IOCFG1       0x01        // GDO1 output pin configuration
-#define CC1101_IOCFG0D      0x02        // GDO0 output pin configuration
-#define CC1101_FIFOTHR      0x03        // RX FIFO and TX FIFO thresholds
-#define CC1101_SYNC1        0x04        // Sync word, high byte
-#define CC1101_SYNC0        0x05        // Sync word, low byte
-#define CC1101_PKTLEN       0x06        // Packet length
-#define CC1101_PKTCTRL1     0x07        // Packet automation control
-#define CC1101_PKTCTRL0     0x08        // Packet automation control
-#define CC1101_ADDR         0x09        // Device address
-#define CC1101_CHANNR       0x0A        // Channel number
-#define CC1101_FSCTRL1      0x0B        // Frequency synthesizer control
-#define CC1101_FSCTRL0      0x0C        // Frequency synthesizer control
-#define CC1101_FREQ2        0x0D        // Frequency control word, high byte
-#define CC1101_FREQ1        0x0E        // Frequency control word, middle byte
-#define CC1101_FREQ0        0x0F        // Frequency control word, low byte
-#define CC1101_MDMCFG4      0x10        // Modem configuration
-#define CC1101_MDMCFG3      0x11        // Modem configuration
-#define CC1101_MDMCFG2      0x12        // Modem configuration
-#define CC1101_MDMCFG1      0x13        // Modem configuration
-#define CC1101_MDMCFG0      0x14        // Modem configuration
-#define CC1101_DEVIATN      0x15        // Modem deviation setting
-#define CC1101_MCSM2        0x16        // Main Radio Control State Machine configuration
-#define CC1101_MCSM1        0x17        // Main Radio Control State Machine configuration
+#define CC1101_IOCFG2       0x29        // GDO2 output pin configuration
+#define CC1101_IOCFG1       0x2E        // GDO1 output pin configuration
+#define CC1101_IOCFG0D      0x06        // GDO0 output pin configuration
+#define CC1101_FIFOTHR      0x07        // RX FIFO and TX FIFO thresholds
+#define CC1101_SYNC1        0xD3        // Sync word, high byte
+#define CC1101_SYNC0        0x91        // Sync word, low byte
+#define CC1101_PKTLEN       0xFF        // Packet length
+#define CC1101_PKTCTRL1     0x04        // Packet automation control
+#define CC1101_PKTCTRL0     0x05        // Packet automation control
+#define CC1101_ADDR         0x00        // Device address
+#define CC1101_CHANNR       0x14        // Channel number
+#define CC1101_FSCTRL1      0x0C        // Frequency synthesizer control
+#define CC1101_FSCTRL0      0x00        // Frequency synthesizer control
+#define CC1101_FREQ2        0x22        // Frequency control word, high byte
+#define CC1101_FREQ1        0xB1        // Frequency control word, middle byte
+#define CC1101_FREQ0        0x3B        // Frequency control word, low byte
+#define CC1101_MDMCFG4      0x2D        // Modem configuration
+#define CC1101_MDMCFG3      0x3B        // Modem configuration
+#define CC1101_MDMCFG2      0x13        // Modem configuration
+#define CC1101_MDMCFG1      0x22        // Modem configuration
+#define CC1101_MDMCFG0      0xF8        // Modem configuration
+#define CC1101_DEVIATN      0x62        // Modem deviation setting
+#define CC1101_MCSM2        0x07        // Main Radio Control State Machine configuration
+#define CC1101_MCSM1        0x30        // Main Radio Control State Machine configuration
 #define CC1101_MCSM0        0x18        // Main Radio Control State Machine configuration
-#define CC1101_FOCCFG       0x19        // Frequency Offset Compensation configuration
-#define CC1101_BSCFG        0x1A        // Bit Synchronization configuration
-#define CC1101_AGCCTRL2     0x1B        // AGC control
-#define CC1101_AGCCTRL1     0x1C        // AGC control
-#define CC1101_AGCCTRL0     0x1D        // AGC control
-#define CC1101_WOREVT1      0x1E        // High byte Event 0 timeout
-#define CC1101_WOREVT0      0x1F        // Low byte Event 0 timeout
-#define CC1101_WORCTRL      0x20        // Wake On Radio control
-#define CC1101_FREND1       0x21        // Front end RX configuration
-#define CC1101_FREND0       0x22        // Front end TX configuration
-#define CC1101_FSCAL3       0x23        // Frequency synthesizer calibration
-#define CC1101_FSCAL2       0x24        // Frequency synthesizer calibration
-#define CC1101_FSCAL1       0x25        // Frequency synthesizer calibration
-#define CC1101_FSCAL0       0x26        // Frequency synthesizer calibration
-#define CC1101_RCCTRL1      0x27        // RC oscillator configuration
-#define CC1101_RCCTRL0      0x28        // RC oscillator configuration
-#define CC1101_FSTEST       0x29        // Frequency synthesizer calibration control
-#define CC1101_PTEST        0x2A        // Production test
-#define CC1101_AGCTEST      0x2B        // AGC test
-#define CC1101_TEST2        0x2C        // Various test settings
-#define CC1101_TEST1        0x2D        // Various test settings
-#define CC1101_TEST0        0x2E        // Various test settings
+#define CC1101_FOCCFG       0x1D        // Frequency Offset Compensation configuration
+#define CC1101_BSCFG        0x1C        // Bit Synchronization configuration
+#define CC1101_AGCCTRL2     0xC7        // AGC control
+#define CC1101_AGCCTRL1     0x00        // AGC control
+#define CC1101_AGCCTRL0     0xB0        // AGC control
+#define CC1101_WOREVT1      0x87        // High byte Event 0 timeout
+#define CC1101_WOREVT0      0x6B        // Low byte Event 0 timeout
+#define CC1101_WORCTRL      0xFB        // Wake On Radio control
+#define CC1101_FREND1       0xB6        // Front end RX configuration
+#define CC1101_FREND0       0x10        // Front end TX configuration
+#define CC1101_FSCAL3       0xEA        // Frequency synthesizer calibration
+#define CC1101_FSCAL2       0x2A        // Frequency synthesizer calibration
+#define CC1101_FSCAL1       0x00        // Frequency synthesizer calibration
+#define CC1101_FSCAL0       0x1F        // Frequency synthesizer calibration
+#define CC1101_RCCTRL1      0x41        // RC oscillator configuration
+#define CC1101_RCCTRL0      0x00        // RC oscillator configuration
+#define CC1101_FSTEST       0x59        // Frequency synthesizer calibration control
+#define CC1101_PTEST        0x7F        // Production test
+#define CC1101_AGCTEST      0x3F        // AGC test
+#define CC1101_TEST2        0x88       // Various test settings
+#define CC1101_TEST1        0x31        // Various test settings
+#define CC1101_TEST0        0x09        // Various test settings
 
 // Strobe commands
 #define CC1101_SRES         0x30        // Reset chip.
@@ -106,3 +106,5 @@ void CCStrobe(char addr)
 void CCSetup() // configure registers
 void CCsetPA(char paIndex) // set power amplification using a table
 char CCRSSIdecode(char rssiEnc) // decode RSSI value
+
+#endif
