0a76498b693d05b294da7037c635427b009aa346
Edward S. <edshin95@gmail.com>
2013-06-30 23:22:05 -0400

    Moved radioCore to /src

--- Quadrotor Firmware/USER_APPLICATION1/src/radioCore.c
+++ Quadrotor Firmware/USER_APPLICATION1/src/radioCore.c
@@ -11,7 +11,7 @@ void transmitData(char *txData, char len, char srcAddress, char destAddress)
 	while(1)
 	{
 		char size;
-		stat = CCRead(CC1101_TXBYTES, &size);
+		CCRead(CC1101_TXBYTES, &size);
 		if( size == 0 )
 			break;
 		else
@@ -24,31 +24,32 @@ char txFifoFree()
 	char stat;
 	char size;
 	
-	stat = CCRead(CC1101_TXBYTES, &size);
+	CCRead(CC1101_TXBYTES, &size);
 	if (size >= 64)
 	{
 		CCStrobe(CC1101_SFTX);
-		stat = CCRead(CC1101_TXBYTES, &size);
+		CCRead(CC1101_TXBYTES, &size);
 	}
 	return (CC1101_FIFO_SIZE - size);
 }
 
 int receiveData(char *rxData, char *len, char *srcAddress, char *destAddress, char *rssi, char *lqi)
 {
-	stat = CCRead(CC1101_RXFIFO, len);
+	CCRead(CC1101_RXFIFO, len);
 	CCRead(CC1101_RXFIFO, destAddress);
 	CCRead(CC1101_RXFIFO, srcAddress);
 	*len -= 2;
 	CCReadBurst(CC1101_RXFIFO, rxData, *len);
 	CCRead(CC1101_RXFIFO, rssi);
 	*rssi = CCRSSIdecode(*rssi);
-	stat = CCRead(CC1101_RXFIFO, lqi);
+	CCRead(CC1101_RXFIFO, lqi);
 	
 	if((*lqi & 0x80) == 0)
 		return NOTHING;
 	*lqi = *lqi & 0x7F;
 	
-	if ((stat & 0xF0) == 0x60){ 
+	if ((stat & 0xF0) == 0x60)
+	{ 
 		errNo=3; //Error RX overflow
 		CCStrobe(CC1101_SFRX); // flush the RX buffer
 		return ERR;
