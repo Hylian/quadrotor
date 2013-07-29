f26589398e4f534472ed70579f96e9197bd625c6
Edward S. <edshin95@gmail.com>
2013-07-30 00:02:22 +0900

    Updated TWI.h function prototype to match TWI.c

--- Quadrotor Firmware/USER_APPLICATION1/src/TWI.h
+++ Quadrotor Firmware/USER_APPLICATION1/src/TWI.h
@@ -1,2 +1,2 @@
-status_code_t twiWrite(char busAddress, char writeAddress, char* dataPtr, char length);
+status_code_t twiWrite(char busAddress, char writeAddress, char data, char length);
 status_code_t twiRead(char busAddress, char readAddress, char* dataPtr, char length);
