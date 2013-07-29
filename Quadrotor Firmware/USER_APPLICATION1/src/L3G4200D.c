17c4876f5b97db83ee97f67111595f59b0ebd516
Edward S. <edshin95@gmail.com>
2013-07-30 00:02:37 +0900

    Initial commit of gyro library

---
+++ Quadrotor Firmware/USER_APPLICATION1/src/L3G4200D.c
@@ -0,0 +1,28 @@
+#include "L3G4200D.h"
+#include "TWICfg.h"
+#include "TWI.h"
+
+void gyroSetup(void)
+{
+	#ifndef _TWI_INITIALIZED
+	#define _TWI_INITIALIZED
+	twi_init();
+	#endif
+	twiWrite(GYRO_ADDR, CTRL_REG1, 0b01011111, 1);
+	/*
+		Datasheet Page: 29
+		ODR: 200 Hz
+		BW Cut-Off: 25
+		Power Down: Normal Mode (1)
+		Xen, Yen, Zen: 1
+	*/
+}
+
+void gyroRead(int* xPtr, int* yPtr, int* zPtr)
+{
+	char temp_data[6];
+	while(twiRead(GYRO_ADDR, OUT_X_L, *temp_data, 6) != STATUS_OK);
+	&xPtr = temp_data[0] + temp_data[1] * 256;
+	&yPtr = temp_data[2] + temp_data[3] * 256;
+	&zPtr = temp_data[4] + temp_data[5] * 256;
+}
